4704a455e6db3771a7dbca850de22595275a245c
Yume <hei.hajatiana@gmail.com>
2023-05-06 15:33:41 +0300

    feat: add field validation

--- src/modules/auth/components/SignUp.tsx
+++ src/modules/auth/components/SignUp.tsx
@@ -1,8 +1,11 @@
-import styles from "./auth.module.css";
-import {RoundedBox} from "@/src/ui/common/box";
-import dogeImage from "@/src/assets/img/doge-meme-icon.jpg";
 import {Avatar, Box, Button, TextField, Typography} from "@mui/material";
+import {useForm} from "react-hook-form";
+import {object, string} from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {RoundedBox} from "@/src/ui/common/box";
 import Image from "next/image";
+import styles from "./auth.module.css";
+import dogeImage from "@/src/assets/img/doge-meme-icon.jpg";
 
 const common = {
   variant: "outlined" as "outlined",
@@ -11,31 +14,73 @@ const common = {
   },
 };
 
+const schema = object({
+  name: string().min(8).required(),
+  email: string().email().required(),
+  password: string().min(8),
+}).required();
+
 export const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (values: any) => {
+    console.log(errors);
+    console.log("values> ", values);
+  };
+
   return (
     <RoundedBox>
-      <Box className={styles.login_container}>
-        <Box className={styles.login_head}>
-          <Avatar>
-            <Image src={dogeImage} alt="doge meme icon" fill />
-          </Avatar>
-          <Typography
-            variant="h4"
-            fontWeight="bold"
-            mt={2}
-          >{`First, tell us who you are`}</Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box className={styles.login_container}>
+          <Box className={styles.login_head}>
+            <Avatar>
+              <Image src={dogeImage} alt="doge meme icon" fill />
+            </Avatar>
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              mt={2}
+            >{`First, tell us who you are`}</Typography>
+          </Box>
+          <Box className={styles.login_body}>
+            <TextField
+              size="medium"
+              placeholder="Name"
+              error={!!errors.name?.message}
+              {...common}
+              {...register("name", {required: true})}
+              helperText={<>{errors.name?.message}</>}
+            />
+            <TextField
+              size="medium"
+              placeholder="Email"
+              error={!!errors.email?.message}
+              {...common}
+              {...register("email", {required: true})}
+              helperText={<>{errors.email?.message}</>}
+            />
+            <TextField
+              size="medium"
+              placeholder="Password"
+              error={!!errors.password?.message}
+              {...common}
+              {...register("password", {required: true})}
+              helperText={<>{errors.password?.message}</>}
+            />
+          </Box>
+          <Box className={styles.login_actions}>
+            <Button size="large" {...common} variant="contained" disableRipple type="submit">
+              Sign up
+            </Button>
+          </Box>
         </Box>
-        <Box className={styles.login_body}>
-          <TextField size="medium" placeholder="Name" {...common} />
-          <TextField size="medium" placeholder="Email" type="email" {...common} />
-          <TextField size="medium" placeholder="Password" type="password" {...common} />
-        </Box>
-        <Box className={styles.login_actions}>
-          <Button size="large" {...common} variant="contained" disableRipple>
-            Sign up
-          </Button>
-        </Box>
-      </Box>
+      </form>
     </RoundedBox>
   );
 };
