7fb07a07a5156a580a6f6adee63f40034d05c05a
Yume <hei.hajatiana@gmail.com>
2023-05-06 16:07:03 +0300

    feat: add useAuth while ensuring its method only get called when it's available

--- src/modules/auth/hooks/useAuth.ts
+++ src/modules/auth/hooks/useAuth.ts
@@ -1,17 +1,18 @@
 import {AUTH_TOKEN_KEY} from "@/src/modules/auth/constants";
 import {AuthPayload} from "@/src/modules/auth/types";
 import {toToken} from "@/src/modules/auth/utils/toToken";
+import {isWindowObjectAvailable} from "@/src/utils/window";
 
 export const useAuth = () => {
   return {
     authenticate: (payload: AuthPayload) => {
-      localStorage.setItem(AUTH_TOKEN_KEY, toToken(payload));
+      isWindowObjectAvailable() && localStorage.setItem(AUTH_TOKEN_KEY, toToken(payload));
     },
     getToken: () => {
-      return localStorage.getItem(AUTH_TOKEN_KEY);
+      return isWindowObjectAvailable() && localStorage.getItem(AUTH_TOKEN_KEY);
     },
     clearToken: () => {
-      return localStorage.removeItem(AUTH_TOKEN_KEY);
+      return isWindowObjectAvailable() && localStorage.removeItem(AUTH_TOKEN_KEY);
     },
   };
 };
