2f8e85244ac657dc21ac959dc9f1a00bffa04bbf
Yume <hei.hajatiana@gmail.com>
2023-05-08 22:50:27 +0300

    refactor: rename className

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -2,5 +2,5 @@ import {PropsWithChildren} from "react";
 import styles from "./layout.module.css";
 
 export const Layout = ({children}: PropsWithChildren) => {
-  return <main className={styles.main_layout}>{children}</main>;
+  return <main className={styles.base_layout}>{children}</main>;
 };
