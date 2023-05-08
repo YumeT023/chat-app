b635b6d9329140420f46168e655353b26ee4d6bf
Yume <hei.hajatiana@gmail.com>
2023-05-08 22:33:33 +0300

    feat: add room page

--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -1,9 +1,12 @@
 import {Authenticated} from "@/src/modules/auth/utils/Authenticated";
+import {HeadController} from "@/src/ui/utils";
 
 const RoomPage = () => {
   return (
     <Authenticated>
-      <h1>Hello world</h1>
+      <HeadController title="chat . rooms">
+        <h1>Hello world</h1>
+      </HeadController>
     </Authenticated>
   );
 };
