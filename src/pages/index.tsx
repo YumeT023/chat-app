e0b3ad10708f1f37ff94f028bf517e2536040269
Yume <hei.hajatiana@gmail.com>
2023-05-08 22:06:10 +0300

    feat: add welcome page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import {Avatar, Stack, Typography} from "@mui/material";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 import {HeadController} from "@/src/ui/utils";
+import {RoundedBox} from "@/src/ui/common/box";
+import dogeImage from "@/src/assets/img/doge-meme-icon.jpg";
+import styles from "@/src/styles/index.module.css";
+import Image from "next/image";
 
-export default function Home() {
-  return <HeadController title="chat . home"></HeadController>;
-}
+const HomePage = () => {
+  const {push} = useRouter();
+
+  useEffect(() => {
+    push("/room");
+  }, [push]);
+
+  return (
+    <HeadController title="chat . home">
+      <RoundedBox>
+        <Stack className={styles.redirect_message_container}>
+          <Avatar>
+            <Image src={dogeImage} alt="doge meme icon" fill />
+          </Avatar>
+
+          <Typography variant="h4" fontWeight="bold" color="primary">{`Welcome back`}</Typography>
+        </Stack>
+      </RoundedBox>
+    </HeadController>
+  );
+};
+
+export default HomePage;
