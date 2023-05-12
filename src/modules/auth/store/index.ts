c9aeba40992e742ebd1468e58c1198391528b237
Yume <hei.hajatiana@gmail.com>
2023-05-12 23:30:29 +0300

    fixup! chore: add authStore

--- src/modules/auth/store/index.ts
+++ src/modules/auth/store/index.ts
@@ -1,23 +1,35 @@
 import {create} from "zustand";
+import {createJSONStorage, persist} from "zustand/middleware";
 import {Nullable} from "@/src/utils/optional";
-import {LoggedUser, Payload} from "@/src/modules/auth/types";
+import {Payload} from "@/src/modules/auth/types";
+import {User} from "@/src/modules/user/types";
+import {AUTH_TOKEN_KEY} from "@/src/modules/auth/constants";
 import {loginWithEmailAndPassword} from "@/src/modules/auth/store/apiService";
 
 type State = {
-  loggedUser: Nullable<LoggedUser>;
+  loggedUser: Nullable<User>;
 };
 type Actions = {
   loginUser: (payload: Payload) => Promise<void>;
   logoutUser: () => void;
 };
 
-export const useAuthStore = create<State & Actions>((set) => ({
-  loggedUser: null,
-  loginUser: async ({email, password}) => {
-    const user = await loginWithEmailAndPassword(email, password);
-    set({loggedUser: user?.data});
-  },
-  logoutUser: () => {
-    set({loggedUser: null});
-  },
-}));
+export const useAuthStore = create<State & Actions>()(
+  persist(
+    (set) => ({
+      loggedUser: null,
+      loginUser: async ({email, password}) => {
+        const user = await loginWithEmailAndPassword(email, password);
+        set({loggedUser: user?.data});
+      },
+      logoutUser: () => {
+        set({loggedUser: null});
+      },
+    }),
+    {
+      name: AUTH_TOKEN_KEY,
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({loggedUser: state.loggedUser}),
+    }
+  )
+);
