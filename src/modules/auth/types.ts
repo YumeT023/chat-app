928a2e5946272407b22f97fe4e237b316fbdc98d
Yume <hei.hajatiana@gmail.com>
2023-05-12 23:58:42 +0300

    chore: remove dead code

--- src/modules/auth/types.ts
+++ src/modules/auth/types.ts
@@ -1,12 +1,5 @@
 import {User} from "@/src/modules/user/types";
 
-// TODO: to be removed
-export type AuthPayload = {
-  name: string;
-  password: string;
-  email: string;
-};
-
 export type LoggedUser = User & {
   token: string;
 };
--- src/modules/auth/utils/toToken.ts
+++
@@ -1,5 +0,0 @@
-import {AuthPayload} from "@/src/modules/auth/types";
-
-export const toToken = ({name, email, password}: AuthPayload) => {
-  return `${name}@${email}:${password}`;
-};
