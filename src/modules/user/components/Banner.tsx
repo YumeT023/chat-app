e58aad5ac17f9a47cd648a369fcdcefe4794c3a1
Yume <hei.hajatiana@gmail.com>
2023-05-17 00:30:06 +0300

    feat: status display

--- src/modules/user/components/Banner.tsx
+++ src/modules/user/components/Banner.tsx
@@ -1,19 +1,24 @@
-import {Avatar} from "@/src/ui/avatar";
 import {StaticImageData} from "next/image";
 import doge from "@/src/assets/img/doge-meme-icon.jpg";
+import {UserAvatar} from "@/src/modules/user";
 
 export type BannerProps = {
   img?: string | StaticImageData;
   bio?: string;
+  statusIndication: "0" | "1" | "2";
   name: string;
 };
 
-export const Banner = ({name, bio, img = doge}: BannerProps) => {
+export const Banner = ({name, statusIndication, bio, img = doge}: BannerProps) => {
   return (
     <div className="w-full">
       <div className="h-24 relative bg-amber-400 mb-10">
         <div className="absolute top-1/3 left-1/2 h-24 w-24 transform -translate-x-1/2 sm:translate-x-0 sm:left-28">
-          <Avatar src={img} className="h-full w-full border-2 border-amber-700 rounded-full" />
+          <UserAvatar
+            src={img}
+            className="h-full w-full border-2 border-gray-50 rounded-full"
+            statusIndex={statusIndication}
+          />
         </div>
       </div>
 
