1b6c708b7946f80f0494b11b0e9401441c951851
Yume <hei.hajatiana@gmail.com>
2023-05-16 23:14:02 +0300

    fix: add cn to avatar root allowing it to scale

--- src/modules/layout/components/SidePanel.tsx
+++ src/modules/layout/components/SidePanel.tsx
@@ -5,8 +5,8 @@ import doge from "@/src/assets/img/doge-meme-icon.jpg";
 export const SidePanel = () => {
   return (
     <SidebarPanelContainer className="bg-gray-800">
-      <div className="flex items-center mb-5 ml-1.5">
-        <Avatar src={doge} className="h-10 w-10 mt-2" />
+      <div className="flex items-center my-4 ml-1.5">
+        <Avatar src={doge} className="h-9 w-9 mr-2 mt-0" />
         <span className="text-xl text-white font-semibold">Sleek</span>
       </div>
     </SidebarPanelContainer>
