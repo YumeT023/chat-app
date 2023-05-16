b67db5785dc75ef07cbb832b810e8c1d2014892d
Yume <hei.hajatiana@gmail.com>
2023-05-16 23:51:47 +0300

    feat: add main profile info

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,12 @@
 import {MainGrid, SidePanel} from "@/src/modules/layout";
 import {Authenticated} from "@/src/modules/auth/utils/Authenticated";
+import {Profile} from "@/src/modules/user";
 
 const ViewProfilePage = () => {
   return (
     <Authenticated>
       <MainGrid sidePanel={<SidePanel />} rootClass="flex items-center justify-center h-full">
-        <div className="text-2xl">Profile page</div>
+        <Profile />
       </MainGrid>
     </Authenticated>
   );
