5ef4724a148868e5ec829d67b4a33595c2daf720
Yume <hei.hajatiana@gmail.com>
2023-05-18 16:32:02 +0300

    feat: add slack color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#4a154b",
+          200: "#f4ede4",
+          dark: "#1d1c1d",
+          white: "#fff",
+        },
+        accent: {
+          100: "#36c5f0",
+          200: "#2eb67d",
+          300: "#ecb22e",
+          400: "#e01e5a",
+        },
+      },
+    },
   },
   plugins: [],
 };
