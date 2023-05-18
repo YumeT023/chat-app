863a388a15334985d5d02e34f9a0e0e62cf024ed
Yume <hei.hajatiana@gmail.com>
2023-05-18 17:39:53 +0300

    feat: add 'larsseit' font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import "@/src/styles/globals.css";
 import {MainContainer} from "../modules/layout";
 import type {AppProps} from "next/app";
+import localFont from "next/font/local";
+
+const larsseit = localFont({
+  src: [
+    {
+      path: "../assets/font/larsseit/Larsseit.woff2",
+      weight: "400",
+    },
+    {
+      path: "../assets/font/larsseit/Larsseit-Bold.woff2",
+      weight: "700",
+    },
+    {
+      path: "../assets/font/larsseit/Larsseit-Light.woff2",
+      weight: "200",
+    },
+  ],
+});
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <MainContainer>
+    <MainContainer className={larsseit.className}>
       <Component {...pageProps} />
     </MainContainer>
   );
