abe6621bd015ef81ad91b8add31d31202ab4db32
Yume <hei.hajatiana@gmail.com>
2023-05-19 23:26:18 +0300

    feat: add Input variant and size prop

--- src/ui/form/InputField.tsx
+++ src/ui/form/InputField.tsx
@@ -1,16 +1,48 @@
 import {ComponentPropsWithoutRef, forwardRef} from "react";
 
+const sizeClasses = {
+  lg: "p-4",
+  md: "p-2.5",
+};
+
+const variantClasses = {
+  dark: "border-gray-600 bg-dark-100 text-gray-400 placeholder-gray-600",
+  light: "placeholder-gray-300 focus:border-primary",
+};
+
 export type InputFieldProps = {
+  name: string;
   error?: string;
   root?: string;
+  explicitName?: boolean;
+  labelCls?: string;
+  sizeVariant?: keyof typeof sizeClasses;
+  variant?: keyof typeof variantClasses;
 } & ComponentPropsWithoutRef<"input">;
 
 export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
-  ({className, root = "", error = null, ...props}, ref) => {
-    const cn = `w-full rounded-md border py-4 text-sm px-4  placeholder-gray-300 outline-none focus:border-primary focus-visible:shadow-none ${className}`;
+  (
+    {
+      className,
+      name,
+      explicitName,
+      sizeVariant = "lg",
+      root = "",
+      labelCls = "",
+      error = null,
+      variant = "light",
+      ...props
+    },
+    ref
+  ) => {
+    const size = sizeClasses[sizeVariant];
+    const mode = variantClasses[variant];
+    const cn = `w-full rounded-md border text-sm outline-none focus-visible:shadow-none ${size} ${mode} ${className}`;
+
     return (
       <div className={root}>
-        <input ref={ref} className={cn} data-testid="input-field" {...props} />
+        {explicitName && <div className={`pb-2 capitalize ${labelCls}`}>{name}</div>}
+        <input ref={ref} className={cn} data-testid="input-field" {...props} name={name} />
         {error !== null && <div className="px-2 py-2 text-sm text-red-600">{error}</div>}
       </div>
     );
