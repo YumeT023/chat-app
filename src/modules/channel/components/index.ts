16f26cc5133d513cdc9151ec4e79967171608f3d
Yume <hei.hajatiana@gmail.com>
2023-05-20 04:36:29 +0300

    refactor: (message) -> /components/message

--- src/modules/channel/components/index.ts
+++ src/modules/channel/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./WhatIsChannel";
 export * from "./CreateChannel";
 export * from "./EditChannel";
-export * from "./ChannelMessage";
-export * from "./UserMessage";
+export * from "./message";
--- src/modules/channel/components/message/ChannelMessage.tsx
+++ src/modules/channel/components/message/ChannelMessage.tsx
@@ -58,8 +58,14 @@ Hello world from here lorem2
 
 export const ChannelMessage = () => {
   return (
-    <div className="h-full bg-dark-100 px-3">
-      <UserMessage content={mockContent} />
+    <div className="flex h-full flex-col bg-dark-100">
+      <div className="overflow-y-auto px-3">
+        <UserMessage content={mockContent} />
+      </div>
+
+      <div className="h-48 bg-primary-200">
+        
+      </div>
     </div>
   );
 };
