cb0164fcf4e532fbb723c2abc885c9bf4865311d
Yume <hei.hajatiana@gmail.com>
2023-05-26 10:40:56 +0300

    feat: handle empty message

--- src/modules/channel/components/message/ChannelMessage.tsx
+++ src/modules/channel/components/message/ChannelMessage.tsx
@@ -1,4 +1,4 @@
-import {MessageInput, MessageCard} from "@/src/modules/channel";
+import {MessageCard, MessageInput} from "@/src/modules/channel";
 import {Message} from "@/src/modules/channel/types";
 import {AuthenticatedUser} from "@/src/modules/user/types";
 
@@ -11,9 +11,13 @@ export const ChannelMessage = ({messages, user}: ChannelMessageProps) => {
   return (
     <div className="h-full bg-dark-100">
       <div className="h-3/4 overflow-y-auto px-3">
-        {messages.map((message) => (
-          <MessageCard key={message.id} message={message} self={user} />
-        ))}
+        {messages.length ? (
+          messages.map((message) => <MessageCard key={message.id} message={message} self={user} />)
+        ) : (
+          <div className="p-4 text-sm  text-primary-200 text-opacity-80">
+            No message in this channel
+          </div>
+        )}
       </div>
 
       <div className="h-1/4 p-2">
