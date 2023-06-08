3f471cb571ae3228c2f04101a9745910900687c7
Yume <hei.hajatiana@gmail.com>
2023-06-08 23:20:09 +0300

    feat: add main menu

--- src/modules/layout/components/SidePanel.tsx
+++ src/modules/layout/components/SidePanel.tsx
@@ -1,38 +1,48 @@
 import Link from "next/link";
-import {VscRepoPull} from "react-icons/vsc";
-import {MdPerson} from "react-icons/md";
 import {SidebarPanelContainer} from "@/src/modules/layout";
 import {Avatar} from "@/src/ui/avatar";
 import doge from "@/src/assets/img/doge-meme-icon.jpg";
+import {SelectionBackdrop} from "@/src/ui/box";
+import {CollapsibleMenu} from "@/src/ui/menu";
+import {useRouter} from "next/router";
 
 export type SidePanelProps = {
   username: string;
 };
 
 export const SidePanel = ({username}: SidePanelProps) => {
+  const {route} = useRouter();
+
   return (
-    <SidebarPanelContainer className="bg-dark-100">
+    <SidebarPanelContainer className="bg-dark-100 text-gray-300 ">
       <div className="ml-1.5 flex h-14 items-center border-y border-y-dark-300">
         <Avatar src={doge} className="mr-2 mt-0 h-9 w-9" />
         <span className="text-xl font-semibold text-white">{username}</span>
       </div>
 
       <div className="mt-4 flex flex-col">
-        <Link
-          href="/profile"
-          className="mb-1 flex w-full items-center gap-3 border-b border-b-dark-300 px-2 py-2 text-primary-200 text-opacity-90 hover:text-opacity-100"
+        <CollapsibleMenu
+          header={{
+            label: "Channels",
+            href: "/channel",
+          }}
+          className="mb-2"
+          expandedByDefault={route === "/channels"}
         >
-          <MdPerson className="h-6 w-6" />
-          <div>Profile</div>
-        </Link>
+          <SelectionBackdrop className="w-full py-0.5 text-left">
+            <Link href="/profile">Random</Link>
+          </SelectionBackdrop>
+        </CollapsibleMenu>
 
-        <Link
-          href="/channel"
-          className="mb-1 flex w-full items-center gap-3 border-b border-b-dark-300 px-2 py-2 text-primary-200 text-opacity-90 hover:text-opacity-100"
+        <CollapsibleMenu
+          header={{
+            label: "Direct Messages",
+          }}
         >
-          <VscRepoPull className="h-6 w-6" />
-          <div>Channel</div>
-        </Link>
+          <SelectionBackdrop className="w-full py-0.5 text-left">
+            <Link href="/profile">Yume</Link>
+          </SelectionBackdrop>
+        </CollapsibleMenu>
       </div>
     </SidebarPanelContainer>
   );
