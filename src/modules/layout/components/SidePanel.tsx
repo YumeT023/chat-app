07cbe5db76464fb9a999b52250d8c79d1d0c8ecd
Yume <hei.hajatiana@gmail.com>
2023-06-09 00:16:59 +0300

    feat: route to profile by clicking on name

--- src/modules/layout/components/SidePanel.tsx
+++ src/modules/layout/components/SidePanel.tsx
@@ -28,7 +28,11 @@ const SidePanelComponent = ({user}: SidePanelProps) => {
     <SidebarPanelContainer className="bg-dark-100 text-gray-300 ">
       <div className="ml-1.5 flex h-14 items-center border-y border-y-dark-300">
         <Avatar src={doge} className="mr-2 mt-0 h-9 w-9" />
-        <span className="text-xl font-semibold text-white">{user.name}</span>
+        <Link href="/profile">
+          <SelectionBackdrop className="text-xl font-semibold text-white">
+            {user.name}
+          </SelectionBackdrop>
+        </Link>
       </div>
 
       <div className="mt-4 flex flex-col">
